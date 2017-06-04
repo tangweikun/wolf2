05256eb57f45e13559aa8a0caccec33118184bcd
tangweikun <819573105@qq.com>
2017-06-04 15:17:57 +0800

    add app sidebar title

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-sidebar">
+          <div className="App-sidebar-title">KeepLearning</div>
           <MenuItem>Menu Item</MenuItem>
           <MenuItem>Menu Item 2</MenuItem>
         </div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <label>Keep Learning</label>
         </div>
         <div className="App-body">
           body
