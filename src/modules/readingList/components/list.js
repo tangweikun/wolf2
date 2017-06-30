fd02956a40e1ee5566e619df93699ef9992def3c
tangweikun <819573105@qq.com>
2017-06-30 08:37:51 +0800

    :construction: add readingList demo

--- src/modules/readingList/components/list.js
+++ src/modules/readingList/components/list.js
@@ -1,9 +1,40 @@
 import React from 'react'
 
+const formData = [
+  {
+    _id: 'ewrer32',
+    name: 'React引领未来的用户界面开发框架',
+    readAt: new Date('2016-07-03'),
+    createdAt: new Date('2016-09-03'),
+    author: 'twk',
+  },
+  {
+    _id: 'ewrer3asdf2',
+    name: 'JavaScript面向对象编程指南',
+    readAt: new Date('2016-08-03'),
+    createdAt: new Date('2016-09-03'),
+    author: 'twk2',
+  },
+  {
+    _id: 'ewreafgr32',
+    name: 'ES6标准入门',
+    readAt: new Date('2016-08-03'),
+    createdAt: new Date('2016-11-03'),
+    author: 'ruanyifeng',
+  },
+]
+
 export default function() {
   return (
     <div>
-      test
+      {
+        formData.map(({ name, _id, author, readAt }) =>
+          <div key={_id}>
+            <span>{name}</span>
+            <span>{author}</span>
+            <span>{readAt.getTime()}</span>
+          </div>)
+      }
     </div>
   )
 }
