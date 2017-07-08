a9cf36fa2f90f613acd03585604710b4e7956a4c
tangweikun <819573105@qq.com>
2017-07-08 15:19:05 +0800

    separate property line chart

---
+++ src/modules/property/lineChart.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from 'recharts'
+
+import { PROPERTY } from './constants'
+
+const PropertyLineChart = ({ color, dataKey }) => (
+  <LineChart
+    width={1200}
+    height={600}
+    data={PROPERTY}
+    margin={{ top: 30, right: 30, left: 30, bottom: 30 }}
+  >
+    <XAxis dataKey="date" />
+    <YAxis />
+    <CartesianGrid strokeDasharray="3 3" />
+    <Tooltip />
+    <Legend />
+    <Line type="monotone" dataKey={dataKey} stroke={color} />
+  </LineChart>
+)
+
+export default PropertyLineChart
