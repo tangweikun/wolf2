5a867d3bf0324411fdcfccd7f0729f62cd521c52
tangweikun <819573105@qq.com>
2017-07-09 10:06:28 +0800

    :heavy_plus_sign: adding a dependency

--- src/modules/property/index.js
+++ src/modules/property/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Tabs, Tab} from 'material-ui/Tabs'
+import { Tabs, Tab } from 'material-ui/Tabs'
 import PropertyLineChart from './lineChart'
 import { CATEGORY } from './constants'
 import PropertyList from './list'
