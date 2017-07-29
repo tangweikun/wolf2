da81bb8ff6d66f3d61ece1c761a2eb3c7dcffec9
tangweikun <819573105@qq.com>
2017-07-29 17:31:41 +0800

    :lipstick: update the UI of Task

--- src/modules/todoList/components/Task.jsx
+++ src/modules/todoList/components/Task.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
-export default class Task extends React.Component {
-  render() {
-    return (
-      <div style={{ width: '100%', backgroundColor: 'red', height: '50px' }}>
-        {this.props.text}
-      </div>
-    )
-  }
+export default function Task({ text }) {
+  return (
+    <ContainerDiv>
+      {text}
+    </ContainerDiv>
+  )
 }
 
 Task.propTypes = {
   text: PropTypes.string.isRequired,
 }
+
+const ContainerDiv = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #f8f8ff;
+  border-top: 1px dotted #d3d3d3;
+  padding: 10px;
+`
