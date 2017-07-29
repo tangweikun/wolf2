f25fa7ddfaa8264e590c881a139dd3e02c89afa5
tangweikun <819573105@qq.com>
2017-07-29 12:14:09 +0800

    :sparkles: insert new task

--- src/modules/todo/components/CreateTask.jsx
+++ src/modules/todo/components/CreateTask.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 export default class CreateTask extends React.Component {
+  handleInsertTask = () => {
+    const { newTask, changeValue } = this.props
+    axios
+      .post('insertTask', {
+        task: newTask,
+      })
+      .then((response) => {
+        console.log(response)
+        changeValue('newTask', null)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
   render() {
     const { changeValue, newTask } = this.props
     return (
@@ -10,6 +25,7 @@ export default class CreateTask extends React.Component {
           style={{ width: '100%', height: '30px', border: 0, padding: 0 }}
           value={newTask}
           onChange={e => changeValue('newTask', e.target.value)}
+          onBlur={this.handleInsertTask}
         />
       </div>
     )
