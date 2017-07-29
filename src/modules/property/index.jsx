220f2a8a65e6e7c3b054faafe9cd6bad30fd2ed4
tangweikun <819573105@qq.com>
2017-07-29 09:20:19 +0800

    :hammer: move getProperties from list.jsx to index.js

--- src/modules/property/index.jsx
+++ src/modules/property/index.jsx
@@ -1,19 +1,55 @@
 import React from 'react'
 import { Tabs, Tab } from 'material-ui/Tabs'
+import axios from 'axios'
+import moment from 'moment'
+
 import PropertyLineChart from './lineChart'
 import { CATEGORY } from './constants'
 import PropertyList from './list'
 
-const Property = () =>
-  (<Tabs>
-    <Tab label="资产列表">
-      <PropertyList />
-    </Tab>
-    {Object.entries(CATEGORY).map(([key, { text, color }]) =>
-      (<Tab label={text} key={key}>
-        <PropertyLineChart color={color} dataKey={key} />
-      </Tab>),
-    )}
-  </Tabs>)
+export default class Property extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { properties: null }
+  }
 
-export default Property
+  componentDidMount() {
+    axios
+      .get('findProperty', {})
+      .then(response =>
+        this.setState({
+          properties: response.data.map(({ date, income, outlay }) => ({
+            date: moment(date).format('YYYY/MM/DD'),
+            totalAssets: 10000,
+            netIncome: income - outlay,
+            income,
+            outlay,
+          })),
+        }),
+      )
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    if (!this.state.properties) return null
+    console.log(this.state.properties)
+    return (
+      <Tabs>
+        <Tab label="资产列表">
+          <PropertyList properties={this.state.properties} />
+        </Tab>
+        {Object.entries(CATEGORY).map(([key, { text, color }]) =>
+          (<Tab label={text} key={key}>
+            <PropertyLineChart
+              color={color}
+              dataKey={key}
+              properties={this.state.properties}
+            />
+          </Tab>),
+        )}
+      </Tabs>
+    )
+  }
+}
--- src/modules/property/list.jsx
+++ src/modules/property/list.jsx
@@ -1,64 +1,41 @@
 import React from 'react'
-import axios from 'axios'
-import moment from 'moment'
+import PropTypes from 'prop-types'
 
 import { PROPERTY_LABEL } from './constants'
 import './list.css'
 import DialogProperty from './dialog'
 
-export default class PropertyList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { properties: null }
-  }
-
-  componentDidMount() {
-    axios
-      .get('findProperty', {})
-      .then(response => this.setState({ properties: response.data }))
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
-  getProperties = () =>
-    this.state.properties.map(({ date, income, outlay }) => ({
-      date: moment(date).format('YYYY/MM/DD'),
-      totalAssets: 10000,
-      netIncome: income - outlay,
-      income,
-      outlay,
-    }))
-
-  getTr = () =>
-    this.getProperties().map(item =>
-      (<div className="Property-list-tr" key={Math.random()}>
-        {Object.entries(item).map(([key, value]) =>
-          (<div className="Property-list-td" key={key}>
-            {value}
-          </div>),
-        )}
-      </div>),
-    )
-
-  getTh = () =>
-    (<div className="Property-list-th">
-      {Object.values(PROPERTY_LABEL).map(label =>
-        (<div className="Property-list-td" key={Math.random()}>
-          {label}
+const getTr = properties =>
+  properties.map(item =>
+    (<div className="Property-list-tr" key={Math.random()}>
+      {Object.entries(item).map(([key, value]) =>
+        (<div className="Property-list-td" key={key}>
+          {value}
         </div>),
       )}
-    </div>)
+    </div>),
+  )
+const getTh = () =>
+  (<div className="Property-list-th">
+    {Object.values(PROPERTY_LABEL).map(label =>
+      (<div className="Property-list-td" key={Math.random()}>
+        {label}
+      </div>),
+    )}
+  </div>)
 
-  render() {
-    if (!this.state.properties) return null
-
-    return (
-      <div className="Property-list-container">
-        <DialogProperty />
-        {this.getTh()}
-        {this.getTr()}
-      </div>
-    )
-  }
+function PropertyList({ properties }) {
+  return (
+    <div className="Property-list-container">
+      <DialogProperty />
+      {getTh()}
+      {getTr(properties)}
+    </div>
+  )
 }
+
+PropertyList.propTypes = {
+  properties: PropTypes.arrayOf(PropTypes.object),
+}
+
+export default PropertyList
