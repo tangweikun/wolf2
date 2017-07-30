f8a5bae4ba4308294b1eb684f33d5b3e63921871
tangweikun <819573105@qq.com>
2017-07-31 00:06:50 +0800

    :shit:

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL = 'http://test.tangweikun.cn:4000/'
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.post['Content-Type'] =
   'application/x-www-form-urlencoded'
