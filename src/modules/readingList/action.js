8ea7a770753c4fc92c097f518b2b8216fd2d8a01
tangweikun <819573105@qq.com>
2017-08-13 11:03:14 +0800

    :fire: Remove readList

--- src/modules/readingList/action.js
+++
@@ -1,5 +0,0 @@
-export const changeValue = (key, value) => ({
-  type: 'READLINGLIST_CHANGE_VALUE',
-  key,
-  value,
-})
--- src/modules/readingList/containers/index.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux'
-import { changeValue } from '../action'
-import Component from '../components/index.jsx'
-
-const mapStateToProps = ({ readingList }) => ({
-  inputValue: readingList.inputValue,
-})
-
-export default connect(mapStateToProps, { changeValue })(Component)
