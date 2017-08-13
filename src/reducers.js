2baaa03aea02c55fe05f3bde124d09cfded7e81e
tangweikun <819573105@qq.com>
2017-08-13 11:25:38 +0800

    :bug: Fix bug about forgot delete readinglist reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 import todoList from './modules/todoList/reducer'
-import readingList from './modules/readingList/reducer'
 import board from './modules/taskList/reducer'
 
 const rootReducer = combineReducers({
   todoList,
-  readingList,
   board,
   routing: routerReducer,
 })
